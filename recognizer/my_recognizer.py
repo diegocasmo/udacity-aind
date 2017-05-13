5a7cd12bf450fb9fadb061911db113385532abf4
Diego Castillo <diegocasmo@gmail.com>
2017-05-13 17:28:10 -0500

    Implemented '#recognize'

--- recognizer/my_recognizer.py
+++ recognizer/my_recognizer.py
@@ -1,7 +1,6 @@
 import warnings
 from asl_data import SinglesData
 
-
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
 
@@ -20,6 +19,25 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    X_lengths = test_set.get_all_Xlengths()
+    for X, lengths in X_lengths.values():
+        log_l = {} # Save the likelihood of a word
+        max_score = float("-inf") # Save max score as recognizer iterates the list
+        best_guess = None # Save best guess as recognizer iterates the list
+        for word, model in models.items():
+            try:
+                # Score word using model
+                word_score = model.score(X, lengths)
+                log_l[word] = word_score
+
+                if word_score > max_score:
+                    max_score = word_score
+                    best_guess = word
+            except:
+                # Unable to process word
+                log_l[word] = float("-inf")
+
+        guesses.append(best_guess)
+        probabilities.append(log_l)
+
+    return probabilities, guesses
